68e83f789af3a2e932c37d680527b4b21dfe0fd3
vandana1976 <48695272+vandana1976@users.noreply.github.com>
2019-04-15 15:21:49 -0400

    module.exports2

---
+++ Ex2.js
@@ -0,0 +1,14 @@
+
+
+function repeat(operation, num) {
+     function operation(){
+        return num;
+     }
+     return operation
+  }
+
+//   for(repeat()=0; repeat() <= 10; repeat()++){
+//     return ;
+//   }
+ 
+  module.exports = repeat       
